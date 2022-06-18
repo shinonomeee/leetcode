f89c517776884fa92a1a7acef2611d1b17905440
shinonome <chris2423883715@gmail.com>
2022-06-19 00:48:51 +0800

    通用模版

--- src/main/java/cn/shinonome/Main.java
+++ src/main/java/cn/shinonome/Main.java
@@ -7,6 +7,8 @@ package cn.shinonome;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, LeetCode!");
+        Solution s = new Solution();
+
+
     }
 }
---
+++ src/main/java/cn/shinonome/Solution.java
@@ -0,0 +1,9 @@
+package cn.shinonome;
+
+/**
+ * @Description
+ * @Author chris
+ * @Date 2022/6/19, 00:47
+ */
+public class Solution {
+}
