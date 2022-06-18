257bf40dc4c2ce1978be9fddb06398350fd4b190
shinonome <chris2423883715@gmail.com>
2022-06-19 01:46:33 +0800

    508. 出现次数最多的子树元素和

--- src/main/java/cn/shinonome/Main.java
+++ src/main/java/cn/shinonome/Main.java
@@ -1,7 +1,11 @@
 package cn.shinonome;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 /**
- * @Description
+ * @Description 508. 出现次数最多的子树元素和
  * @Author chris
  * @Date 2022/6/19, 00:33
  */
@@ -10,5 +14,68 @@ public class Main {
         Solution s = new Solution();
 
 
+    }
+}
+
+class Solution {
+    private final Map<Integer, Integer> feq = new HashMap<>();
+    private int maxFeq = Integer.MIN_VALUE;
+
+    public int[] findFrequentTreeSum(TreeNode root) {
+        dfs(root);
+        Stack<Integer> st = new Stack<>();
+        for (Map.Entry<Integer, Integer> entry: feq.entrySet()) {
+            int key = entry.getKey();
+            int keyFeq = entry.getValue();
+            if (keyFeq == maxFeq) {
+                st.push(key);
+            }
+        }
+        int[] ans = new int[st.size()];
+        int i = 0;
+        while (!st.isEmpty()) {
+            ans[i++] = st.pop();
+        }
+        return ans;
+    }
+
+    private void dfs(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        int sum = getSum(node);
+        int leftFeq = feq.getOrDefault(sum, 0) + 1;
+        feq.put(sum, leftFeq);
+        maxFeq = Math.max(maxFeq, leftFeq);
+        if (node.left != null) {
+            dfs(node.left);
+        }
+        if (node.right != null) {
+            dfs(node.right);
+        }
+    }
+
+    private int getSum(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return node.val + getSum(node.left) + getSum(node.right);
+    }
+}
+
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {
+    }
+    TreeNode(int val) {
+        this.val = val;
+    }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
