d2d131398cc1bcd6a752bcd5be948432f3fe7434
shinonome <chris2423883715@gmail.com>
2022-06-19 12:35:28 +0800

    TODO:换用邻接表存储

--- src/main/java/cn/shinonome/Main.java
+++ src/main/java/cn/shinonome/Main.java
@@ -1,5 +1,10 @@
 package cn.shinonome;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Queue;
+
 /**
  * @Description TODO
  * @Author chris
@@ -8,11 +13,41 @@ package cn.shinonome;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-
-
+        System.out.println(Arrays.toString(s.findOrder(3, new int[][]{{1, 0}, {1, 2}, {0, 1}})));
     }
 }
 
 class Solution {
-
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[][] matrix = new int[numCourses][numCourses]; // 邻接矩阵
+        int[] penetrations = new int[numCourses];         // 每个顶点的入度
+        Queue<Integer> q = new ArrayDeque<>();            // 工作队列
+        int[] ans = new int[numCourses];
+        int i = 0, index = 0;
+        for (int[] arr : prerequisites) {
+            if (arr[0] == arr[1]) {
+                return new int[]{};
+            }
+            matrix[arr[1]][arr[0]] = 1;
+            ++penetrations[arr[0]];
+        }
+        for (; i < numCourses; ++i) {
+            if (penetrations[i] == 0) {
+                q.offer(i);
+            }
+        }
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            ans[index++] = cur;
+            for (i = 0; i < numCourses; ++i) {
+                if (matrix[cur][i] == 1 && --penetrations[i] == 0) {
+                    q.offer(i);
+                }
+            }
+        }
+        if (index != numCourses) {
+            return new int[]{};
+        }
+        return ans;
+    }
 }
