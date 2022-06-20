df61ed36c1de01aca5fd62697bbdfd0ad3b4600b
shinonome <chris2423883715@gmail.com>
2022-06-21 01:29:42 +0800

    945

--- src/main/java/cn/shinonome/Main.java
+++ src/main/java/cn/shinonome/Main.java
@@ -1,5 +1,9 @@
 package cn.shinonome;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @Description TODO
  * @Author chris
@@ -13,3 +17,16 @@ public class Main {
     }
 }
 
+class Solution {
+    public int minIncrementForUnique(int[] nums) {
+        int count = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] >= nums[i + 1]) {
+                count += nums[i] - nums[i + 1] + 1;
+                nums[i + 1] = nums[i] + 1;
+            }
+        }
+        return count;
+    }
+}
