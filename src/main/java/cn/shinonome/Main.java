6103c7db0c91e23896ed11ab367451d222dedb36
shinonome <chris2423883715@gmail.com>
2022-06-21 01:33:51 +0800

    945

--- src/main/java/cn/shinonome/Main.java
+++ src/main/java/cn/shinonome/Main.java
@@ -5,15 +5,14 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * @Description TODO
+ * @Description 945. 使数组唯一的最小增量
  * @Author chris
  * @Date 2022/6/19, 00:33
  */
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-
-
+        System.out.println(s.minIncrementForUnique(new int[]{3, 2, 1, 2, 1, 7}));
     }
 }
 
