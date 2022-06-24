83faaac828e32f0e0db15771ebfcc3d53b911a04
shinonome <chris2423883715@gmail.com>
2022-06-24 18:21:00 +0800

    515

---
+++ README.md
@@ -0,0 +1,7 @@
+## 515. 在每个树行中找最大值
+> 难度：Medium \
+> [原题链接](https://leetcode.cn/problems/find-largest-value-in-each-tree-row)
+
+给定一棵二叉树的根节点 `root` ，请找出该二叉树中每一层的最大值。
+
+![示例输入](pic/ex1.png)
--- src/main/java/cn/shinonome/Main.java
+++ src/main/java/cn/shinonome/Main.java
@@ -1,15 +1,64 @@
 package cn.shinonome;
 
+import java.util.*;
+
 /**
- * @Description TODO
+ * @Description 515. 在每个树行中找最大值
  * @Author chris
  * @Date 2022/6/19, 00:33
  */
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-
-
+        System.out.println(s.largestValues(null));
     }
 }
 
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        Deque<TreeNode> dq = new ArrayDeque<>();
+        TreeNode last = root;
+        int max = Integer.MIN_VALUE;
+        dq.offer(root);
+        while (!dq.isEmpty()) {
+            TreeNode cur = dq.getFirst();
+            max = Math.max(max, cur.val);
+            if (cur.left != null) {
+                dq.offer(cur.left);
+            }
+            if (cur.right != null) {
+                dq.offer(cur.right);
+            }
+            if (cur == last) {
+                last = dq.getLast();
+                ans.add(max);
+                max = Integer.MIN_VALUE;
+            }
+            dq.poll();
+        }
+        return ans;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
