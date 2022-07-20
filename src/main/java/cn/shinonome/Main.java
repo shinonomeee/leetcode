72d39d2bb6275a1c526ba1c0bf1c8180f0d3f07d
shinonome <shinome@126.com>
2022-07-20 14:30:49 +0800

    .

--- src/main/java/cn/shinonome/Main.java
+++ src/main/java/cn/shinonome/Main.java
@@ -1,5 +1,8 @@
 package cn.shinonome;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Description TODO
  * @Author chris
@@ -8,8 +11,23 @@ package cn.shinonome;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-
+        s.shiftGrid(new int[]{
+                new int[]{1, 2, 3},
+                new int[]{1, 2, 3}}, 2);
 
     }
 }
 
+class Solution {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        int m = grid.length;
+        int n = grid[0].length;
+        k %= (m * n);
+        int shift = k / n;
+        k %= n;
+        List<List<Integer>> ans = new ArrayList<>(m);
+        ans.forEach(e -> e = new ArrayList<>(n));
+        System.out.println(ans);
+        return ans;
+    }
+}
