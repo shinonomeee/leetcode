bd04c83b46c0a3964cf175ec4ae61f5a392b2656
shinonome <shinome@126.com>
2022-07-20 15:16:04 +0800

    1260. 二维网格迁移

--- src/main/java/cn/shinonome/Main.java
+++ src/main/java/cn/shinonome/Main.java
@@ -11,9 +11,11 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        s.shiftGrid(new int[]{
-                new int[]{1, 2, 3},
-                new int[]{1, 2, 3}}, 2);
+        System.out.println(s.shiftGrid(new int[][]{
+                        {1, 2, 3},
+                        {4, 5, 6},
+                        {7, 8, 9}
+                }, 1));
 
     }
 }
@@ -23,11 +25,21 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         k %= (m * n);
-        int shift = k / n;
-        k %= n;
-        List<List<Integer>> ans = new ArrayList<>(m);
-        ans.forEach(e -> e = new ArrayList<>(n));
-        System.out.println(ans);
+        int rowShift = k / n;
+        int colShift = k % n;
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < m; ++i) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j < n; ++j) {
+                row.add(0);
+            }
+            ans.add(row);
+        }
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                ans.get((i + rowShift + (j + colShift) / n) % m).set((j + colShift) % n, grid[i][j]);
+            }
+        }
         return ans;
     }
 }
