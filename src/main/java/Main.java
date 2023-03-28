9905fea0aa15284062163517b8c2dfadf8646bfe
zhangchenyu <shinome@126.com>
2023-03-28 13:38:02 +0800

    clean branch

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @Description TODO
+ * @Author chris
+ * @Date 2022/6/19, 00:33
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+
+    }
+}
