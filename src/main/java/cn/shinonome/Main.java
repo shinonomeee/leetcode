d3a5d726c7c3ab8ebba3a065eb7f6bdcf22be31d
zhangchenyu <shinome@126.com>
2023-03-28 13:35:16 +0800

    1

--- src/main/java/cn/shinonome/Main.java
+++ src/main/java/cn/shinonome/Main.java
@@ -1,5 +1,7 @@
 package cn.shinonome;
 
+import java.util.*;
+
 /**
  * @Description TODO
  * @Author chris
@@ -7,6 +9,35 @@ package cn.shinonome;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, leetcode!");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        boolean[] isVisited = new boolean[10];
+        int ans = backtrack(num, isVisited, new ArrayDeque<>(), Integer.MAX_VALUE);
+        System.out.println(ans);
+    }
+    private static int backtrack(int target, boolean[] isVisited, Deque<Integer> cur, int curMin) {
+        if (target == 0) {
+            int ans = 0;
+            for (int digit : cur) {
+                ans *= 10;
+                ans += digit;
+            }
+            curMin = Math.min(ans / 10, curMin);
+            return curMin;
+        }
+        if (target < 0) {
+            return Integer.MAX_VALUE;
+        }
+        for (int i = 1; i <= 9; ++i) {
+            if (isVisited[i]) {
+                continue;
+            }
+            isVisited[i] = true;
+            cur.addLast(i);
+            backtrack(target - i, isVisited, cur, curMin);
+            cur.pollLast();
+            isVisited[i] = false;
+        }
+        return Integer.MAX_VALUE;
     }
 }
